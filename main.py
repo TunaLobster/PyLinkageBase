f639fc03fbbd1f51cd55c06c852c0171f93a7edd
TunaLobster <charliepjohnson@gmail.com>
2018-10-31 16:21:53 -0500

    Change to QDialog for UI. Add several helper functions from Dela. Update TODO list. Update data file.

--- main.py
+++ main.py
@@ -18,119 +18,202 @@
 # TODO: add ability to draw svg like graphics for coupler/statics
 
 
+import numpy as np
 from OpenGL.GL import *
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QOpenGLWidget, QApplication
+from OpenGL.GLUT import *
+from PyQt5.QtWidgets import QDialog, QApplication
+
+from OpenGL_2D_class import gl2D
+from OpenGL_2D_ui import Ui_Dialog
+
 
 # example data for graphing. not to be used as actual data.
-import helpers
+# import helpers
 
 
-# Basics of OpenGL taken from:
-# http://trevorius.com/scrapbook/uncategorized/part-1-drawing-with-pyopengl-using-moden-opengl-buffers/
-# https://pythonprogramming.net/opengl-rotating-cube-example-pyopengl-tutorial/
-class LinkageOpenGLWidget(QOpenGLWidget):
-    def __init__(self, linkage_data):
-        super(LinkageOpenGLWidget, self).__init__()
-        self.__timer = None
-        self.__vao = None
+class main_window(QDialog):
+    def __init__(self):
+        super(main_window, self).__init__()
+        self.ui = Ui_Dialog()
+        # setup the GUI
+        self.ui.setupUi(self)
 
-        self.linkage_data = linkage_data
+        # define any data (including object variables) your program might need
+        self.showlabels = True
 
-    def initializeGL(self):
-        # set the RGBA values of the background
-        glClearColor(0.1, 0.2, 0.3, 1.0)
-        # set a timer to redraw every 1/60th of a second
-        self.__timer = QTimer()
-        self.__timer.timeout.connect(self.repaint)  # make it repaint when triggered
-        self.__timer.start(1000 / 60)  # make it trigger every 1000/60 milliseconds
+        self.circle1x = -1
+        self.circle1y = -0.5
+        self.circle1radius = 0.2
 
-        # set up for drawing lines (really narrow rectangles because that's how OpenGL rolls
-        positions = (0, 0, 1, 0, 0, 1, 1, 1)
-        elements = (0, 1, 2, 1, 3, 2)
+        self.save_circle1x = 0
+        self.save_circle1y = 0
 
-        # generate a vertex array object so we can easily draw the resulting mesh later
-        self.__vao = glGenVertexArrays(1)
-        # enable the vertex array before doing anything else, so anything we do is captured in the VAO context
-        glBindVertexArray(self.__vao)
-        # generate 2 buffers, 1 for positions, 1 for elements. this is memory on the GPU that our model will be saved in.
-        bufs = glGenBuffers(2)
-        # set the first buffer for the main vertex data, that GL_ARRAY_BUFFER indicates that use case
-        glBindBuffer(GL_ARRAY_BUFFER, bufs[0])
-        # upload the position data to the GPU
-        # some info about the arguments:
-        # GL_ARRAY_BUFFER: this is the buffer we are uploading into, that is why we first had to bind the created buffer, else we'd be uploading to nothing
-        # sizeof(ctypes.c_float) * len(positions): openGL wants our data as raw C pointer, and for that it needs to know the size in bytes.
-        # the ctypes module helps us figure out the size in bytes of a single number, then we just multiply that by the array length
-        # (ctypes.c_float * len(positions))(*positions): this is a way to convert a python list or tuple to a ctypes array of the right data type
-        # internally this makes that data the right binary format
-        # GL_STATIC_DRAW: in OpenGL you can specify what you will be doing with this buffer, static means draw it a lot but never access or alter the data once uploaded.
-        # I suggest changing this only when hitting performance issues at a time you are doing way more complicated things. In general usage static is the fastest.
-        glBufferData(GL_ARRAY_BUFFER, sizeof(ctypes.c_float) * len(positions),
-                     (ctypes.c_float * len(positions))(*positions), GL_STATIC_DRAW)
-        # set the second buffer for the triangulation data, GL_ELEMENT_ARRAY_BUFFER indicates the use here
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufs[1])
-        # upload the triangulation data
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ctypes.c_uint) * len(elements),
-                     (ctypes.c_uint * len(elements))(*elements), GL_STATIC_DRAW)
-        # because the data is now on the GPU, our python positions & elements can be safely garbage collected hereafter
-        # turn on the position attribute so OpenGL starts using our array buffer to read vertex positions from
-        glEnableVertexAttribArray(0)
-        # set the dimensions of the position attribute, so it consumes 2 floats at a time (default is 4)
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
+        self.circle2x = -1
+        self.circle2y = 2
+        self.circle2radius = 0.15
 
-    def resizeGL(self, width, height):
-        # this tells openGL how many pixels it should be drawing into
-        glViewport(0, 0, width, height)
+        # create and setup the GL window object
+        self.setupGLWindows()
 
-    def paintGL(self):
-        # empty the screen, setting only the background color
-        # the depth_buffer_bit also clears the Z-buffer, which is used to make sure
-        # objects that are behind other objects actually are not shown drawing
-        # a faraway object later than a nearby object naively implies that it will
-        # just fill in the pixels with itself, but if there is already an object there
-        # the depth buffer will handle checking if it is closer or not automatically
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        # the openGL window has coordinates from (-1,-1) to (1,1), so this fills in
-        # the top right corner with a rectangle. The default color is white.
-        # glRecti(0, 0, 1, 1)
+        # and define any Widget callbacks or other necessary setup
+        self.assign_widgets()
 
-        # enable the vertex array we initialized, it will bind the right buffers in the background again
-        glBindVertexArray(self.__vao)
-        # draw triangles based on the active GL_ELEMENT_ARRAY_BUFFER
-        # that 6 is the element count, we can save the len(elements) in initializeGL in the future
-        # that None is because openGL allows us to supply an offset for what element to start drawing at
-        # (we could only draw the second triangle by offsetting by 3 indices for example)
-        # problem is that the data type for this must be None or ctypes.c_void_p.
-        # In many C++ example you will see just "0" being passed in
-        # but in PyOpenGL this doesn't work and will result in nothing being drawn.
-        glDrawElements(GL_LINES, 6, GL_UNSIGNED_INT, None)
+        # show the GUI
+        self.show()
 
-    def shape(self):
-        # glBegin(GL_LINES)
-        # for edge in edges:
-        #     for vertex in edge:
-        #         glVertex3fv(vertices[vertex])
-        # glEnd()
-        pass
+    def assign_widgets(self):  # callbacks for Widgets on your GUI
+        self.ui.pushButton_Exit.clicked.connect(self.ExitApp)
+        self.ui.pushButton_Animate.clicked.connect(self.StartAnimation)
+        self.ui.pushButton_StopAnimation.clicked.connect(self.StopAnimation)
+        self.ui.pushButton_ResumeAnimation.clicked.connect(self.ResumeAnimation)
+        self.ui.horizontalSlider_zoom.valueChanged.connect(self.glZoomSlider)
+        self.ui.horizontalSlider_rotate.valueChanged.connect(self.glRotateSlider)
+        self.ui.checkBox_Dragging.stateChanged.connect(self.DraggingToggle)
 
+    # Widget callbacks start here
 
-def main():
-    linkage_data = helpers.Linkage(r'linkageData.txt')
-    linkage_data.parse_data()
-    # for i in linkage_data.nodes:
-    #     print(i)
-    print(linkage_data)
-    app = QApplication([])
-    widget = LinkageOpenGLWidget(linkage_data)
-    widget.resizeGL(100, 100)
-    widget.show()
-    app.exec_()
+    def glZoomSlider(self):  # I used a slider to control GL zooming
+        zoomval = float((self.ui.horizontalSlider_zoom.value()) / 200 + 0.25)
+        self.glwindow1.glZoom(zoomval)  # set the zoom value
+        self.glwindow1.glUpdate()  # update the GL image
+
+    def glRotateSlider(self):  # I used a slider to control GL rotation
+        angle = -float(self.ui.horizontalSlider_rotate.value())
+        self.glwindow1.glRotate(angle, 0.5, 0.5)  # set the rotation angle and centerpoint
+        self.glwindow1.glUpdate()  # update the GL image
+
+    def DraggingToggle(self):  # used a checkbox to Enable GL Dragging
+        if self.ui.checkBox_Dragging.isChecked():  # start dragging
+            # put locations of "draggable" things into a  list
+            draglist = [[self.circle1x, self.circle1y],
+                        [self.circle2x, self.circle2y]]
+            near = 0.1  # define an acceptable mouse distance for dragging
+            self.glwindow1.glStartDragging(self.DraggingCallback, draglist, near,
+                                           handlesize=0.06, handlewidth=3, handlecolor=[1, 0, 0])
+
+        else:  # stop dragging
+            self.glwindow1.glStopDragging()
+
+    def StartAnimation(self):  # a button to start GL Animation
+        # a playlist is a list values defining the look of the picture for
+        # each FRAME in the animation.
+        self.save_circle1x = self.circle1x
+        self.save_circle1y = self.circle1y
+
+        nframes = 121
+        frames = np.linspace(0, nframes - 1, nframes)
+
+        self.glwindow1.glStartAnimation(self.AnimationCallback, frames,
+                                        reverse=True, repeat=False)
+
+    def StopAnimation(self):  # a button to Stop GL Animati0n
+        self.glwindow1.glStopAnimation()
+
+    def ResumeAnimation(self):  # a button to Resume GL Animation
+        self.glwindow1.glResumeAnimation()
+
+    def ExitApp(self):
+        app.exit()
+
+    # Setup OpenGL Drawing and Viewing
+
+    def setupGLWindows(self):  # setup all GL windows
+        # send it the   GL Widget     and the drawing Callback function
+        self.glwindow1 = gl2D(self.ui.openGLWidget, self.DrawingCallback)
+
+        # set the drawing space:    xmin  xmax  ymin   ymax
+        self.glwindow1.setViewSize(-3, 3, -1, 3, allowDistortion=False)
+
+        # Optional: Setup GL Mouse Functionality
+        self.ui.openGLWidget.installEventFilter(self)  # to read mouse events
+        self.ui.openGLWidget.setMouseTracking(True)  # to enable mouse events
+
+        # OPTIONAL: to display the mouse location  - the name of the TextBox
+        self.glwindow1.glMouseDisplayTextBox(self.ui.MouseLocation)
+
+    def eventFilter(self, source, event):  # allow GL to handle Mouse Events
+        self.glwindow1.glHandleMouseEvents(event)  # let GL handle the event
+        return super(QDialog, self).eventFilter(source, event)
+
+    def DrawingCallback(self):
+        # this is what actually draws the picture
+        # using data to control what is drawn
+
+        # Draw Circle 1
+        # glColor3f(0, 1, 0)  #
+        # glLineWidth(1.5)
+        # gl2DCircle(self.circle1x, self.circle1y, self.circle1radius)
+
+        # Draw 3 connected lines
+        glColor3f(0.9, 0.9, 0.25)
+        glLineWidth(4)
+        glBegin(GL_LINE_STRIP)  # begin drawing connected lines
+        # use GL_LINE for drawing a series of disconnected lines
+        glVertex2f(-2, 0)
+        glVertex2f(-2, 2)
+        glVertex2f(2, 2)
+        glVertex2f(2, 0)
+        glEnd()
+
+        # Draw Circle 2
+        # glColor3f(0.8, 1, 0.8)
+        # glLineWidth(1.5)
+        # gl2DCircle(self.circle2x, self.circle2y, self.circle2radius, fill=True)
+
+        # call DrawLines
+        self.DrawLines()
+
+        # if dragging, let GL show dragging handles
+        self.glwindow1.glDraggingShowHandles()
+
+    def AnimationCallback(self, frame, nframes):
+        # a callback function for animation
+        # then, call glUpdate() to update the image
+
+        # change object properties of each node here
+        app.processEvents()  # absolutely required!!!
+
+    def DraggingCallback(self, x, y, draglist, index):
+        if index == 0:  # mouse near circle 1
+            self.circle1x, self.circle1y = [x, y]  # change circle 1 data
+            draglist[index] = [x, y]  # save both x and y back to the draglist
+
+        if index == 1:  # mouse near circle 2 ... only change the x-value
+            self.circle2x = x  # only change the  x-value for circle 2
+            draglist[index][0] = x  # only save the x value to the draglist
+        # end method
+
+    def DrawLines(self):
+        # Draw some lines
+        glColor3f(0, 0.90, 0.25)
+        glLineWidth(4)
+        glBegin(GL_LINE_STRIP)  # begin drawing connected lines
+        # use GL_LINE for drawing a series of disconnected lines
+        glVertex2f(0, 0)
+        glVertex2f(0, 1)
+        glVertex2f(1, 1)
+        glVertex2f(1, 0)
+        glVertex2f(0, 0)
+        glEnd()
+        # change the color and linewidth
+        glColor3f(0.75, 0.15, 0.250)
+        glLineWidth(8)
+        glBegin(GL_LINE_STRIP)  # begin drawing connected lines
+        glVertex2f(0, 1)
+        glVertex2f(0.5, 1.7)
+        glVertex2f(1, 1)
+        glEnd()
 
 
 if __name__ == '__main__':
-    # app = QApplication([])
-    # widget = LinkageOpenGLWidget()
-    # widget.show()
-    # app.exec_()
-    main()
+    # linkage_data = helpers.Linkage(r'linkageData.txt')
+    # linkage_data.parse_data()
+    # for i in linkage_data.nodes:
+    #     print(i)
+    # print(linkage_data)
+    app = QApplication.instance()
+    if not app:
+        app = QApplication(sys.argv)
+    app.aboutToQuit.connect(app.deleteLater)
+    main_win = main_window()
+    sys.exit(app.exec_())
